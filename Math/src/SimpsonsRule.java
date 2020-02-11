c9f22c601b044db8ce61b4dabefecf93018ff380
surfer1011 <gchacka@gmail.com>
2020-02-10 20:00:46 -0800

    updates to simpsons

--- Math/src/SimpsonsRule.java
+++ Math/src/SimpsonsRule.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
 
 public class SimpsonsRule {
 	
-	//test
-	//more test
+	public static void main(String[]args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter Delta X: ");
+		float dx = scan.nextFloat();
+		
+		System.out.print("Enter N: ");
+		int n = scan.nextInt();
+		while(n%2==1) {
+			System.out.print("Uh oh, N must be divisible by 2, try again\nN: ");
+			n = scan.nextInt();
+		}
+		
+		System.out.print("Enter the function: ");
+		String fx = scan.nextLine();
+		
+		
+		
+	}
 
 }
